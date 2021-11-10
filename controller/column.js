b67dde20797a1c6c2b0c03f909c6da91ff4baa11
zouHuaLu <huangjie1234huang@qq.com>
2021-11-10 13:46:10 +0800

    接口完成

--- controller/column.js
+++ controller/column.js
@@ -1,10 +1,25 @@
 const execSql = require('../mySql/execSql')
 
-const getList = () => {
+const getLatestArticles = () => {
+    const sql = 'select * from table_column limit 3'
+    return execSql(sql)
+}
+
+const getAllArticles = () => {
     const sql = 'select * from table_column'
     return execSql(sql)
 }
 
+const getLatestShuoShuo = () => {
+    const sql = 'select * from table_shuoshuo limit 3'
+    return execSql(sql)
+}
+
+const getAllShuoShuo = () => {
+    const sql = 'select * from table_shuoshuo'
+    return execSql(sql)
+}
+
 module.exports = {
-    getList
+    getLatestArticles,getLatestShuoShuo,getAllArticles,getAllShuoShuo
 }
--- routes/column.js
+++ routes/column.js
@@ -1,14 +1,41 @@
 const express = require('express');
-const {getList} = require('../controller/column')
+const {getLatestArticles ,getLatestShuoShuo,getAllArticles,getAllShuoShuo} = require('../controller/column')
 const { successData, failData } = require('../model/resData')
-const getData = require('../utils/getData')
+// const getData = require('../utils/getData')
 
 const router = express.Router()
 
-router.get('/list',async(req,res)=>{
-    const result = await getList().then(sqlData => {
+router.get('/articlesList',async(req,res)=>{
+    const result = await getLatestArticles().then(sqlData => {
         return successData('list',sqlData)
     }).catch(err=>{
+        return failData(err)
+    })
+    res.send(result)
+})
+
+router.get('/allArticles',async(req,res)=>{
+    const result = await getAllArticles().then(data => {
+        return successData('list',data)
+    }).catch(err => {
+        return failData(err)
+    })
+    res.send(result)
+})
+
+router.get('/latestShuoShuo',async(req,res)=>{
+    const result = await getLatestShuoShuo().then(data => {
+        return successData('list',data)
+    }).catch(err => {
+        return failData(err)
+    })
+    res.send(result)
+})
+
+router.get('/allShuoShuo',async(req,res)=>{
+    const result = await getAllShuoShuo().then(data => {
+        return successData('list',data)
+    }).catch(err => {
         return failData(err)
     })
     res.send(result)
